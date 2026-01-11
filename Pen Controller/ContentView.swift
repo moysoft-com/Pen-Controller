584fc91cc3f313d17a7c6d3f372a26a2600d9d1f
Lian <176339768+Lian735@users.noreply.github.com>
2026-01-11 23:28:01 +0100

    Initial Commit

---
+++ Pen Controller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pen Controller
+//
+//  Created by Lian on 11.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pen Controller/Pen_ControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pen_ControllerApp.swift
+//  Pen Controller
+//
+//  Created by Lian on 11.01.26.
+//
+
+import SwiftUI
+
+@main
+struct Pen_ControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
